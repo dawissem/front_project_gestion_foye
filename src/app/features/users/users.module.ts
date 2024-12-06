f22490bf8326198d556ab9cfa00d565b25538a6c
waelhasnaoui1 <waelhasnaoui006@gmail.com>
2024-12-06 19:58:05 +0100

    .

---
+++ src/app/features/users/users.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UsersRoutingModule } from './users-routing.module';
+import { UsersComponent } from './users/users.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { AddDialogComponent } from './dialogs/add-dialog/add-dialog.component';
+
+@NgModule({
+  declarations: [UsersComponent, AddDialogComponent],
+  imports: [
+    CommonModule,
+    UsersRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatSortModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    ReactiveFormsModule,
+  ],
+})
+export class UsersModule {}
